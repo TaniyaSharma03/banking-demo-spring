f983694ef8e5fd180f563a23e6c0ddbcff9b7c2d
Taniya <taniya.sharma@credable.in>
2022-02-21 11:54:36 +0530

    inserting new changes

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,13 +1,17 @@
 package org.example.app;
 
 import org.example.model.Customer;
+import org.example.repository.CustomerRepositoryDB;
+import org.example.repository.CustomerRepositoryStub;
 import org.example.service.DefaultCustomerService;
 
 import java.util.List;
 public class Application {
     public static void main(String[] args) {
+        CustomerRepositoryStub stubRepository = new CustomerRepositoryStub();
+        CustomerRepositoryDB dbRepository = new CustomerRepositoryDB();
 
-            DefaultCustomerService service = new DefaultCustomerService();
+            DefaultCustomerService service = new DefaultCustomerService(dbRepository);
 
             List<Customer> customers = service.getAllCustomers();
 
--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -1,12 +1,18 @@
 package org.example.service;
 
 import org.example.model.Customer;
+import org.example.repository.CustomerRepository;
 import org.example.repository.CustomerRepositoryDB;
 import java.util.List;
 
 public class DefaultCustomerService {
 
-    private CustomerRepositoryDB repository = new CustomerRepositoryDB();
+    //private CustomerRepositoryDB repository = new CustomerRepositoryDB();
+private CustomerRepository repository;
+
+    public DefaultCustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Customer> getAllCustomers() {
         return repository.findAll();
