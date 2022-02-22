d5fc6fa7347822332b99573087766609fe47d022
Taniya <taniya.sharma@credable.in>
2022-02-22 10:06:18 +0530

    Spring application first example

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,32 +1,31 @@
 package org.example.app;
 
+import org.example.AppConfig;
 import org.example.model.Customer;
 import org.example.repository.CustomerRepositoryDB;
 import org.example.repository.CustomerRepositoryStub;
 import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
 
-//        CustomerRepositoryStub stubRepository = new CustomerRepositoryStub();
-//        CustomerRepositoryDB dbRepository = new CustomerRepositoryDB();
-// wiring your application
-        CustomerService service = new DefaultCustomerService(newCustomerRepositoryStub());
-          //  DefaultCustomerService service = new DefaultCustomerService(dbRepository);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-            List<Customer> customers = service.getAllCustomers();
+        CustomerService service = context.getBean("customerService", CustomerService.class);
 
-            customers.forEach(System.out::println);
+//    CustomerService service = new DefaultCustomerService(new CustomerRepositoryDB());
+//    List<Customer> customers = service.getAllCustomersWithStars();
+        List<Customer> customers = service.getAllCustomers();
+
+        customers.forEach(System.out::println);
     }
 
-    public static CustomerRepositoryDB newCustomerRepositoryDB() {
-        return new CustomerRepositoryDB();
-    }
 
-    public static CustomerRepositoryStub newCustomerRepositoryStub() {
-        return new CustomerRepositoryStub();
-    }
 
 }
